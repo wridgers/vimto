3210b6179b65777bc66ff6a632f4912603437127
Will Ridgers <will@mindfuzz.net>
2014-01-18 15:17:02 +0000

    Added .org to update script.

--- tools/update.bat
+++ tools/update.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :: file extensions
-set EXT_LIST=(asc bib c cfg coffee conf cpp css ejs frag fp glsl gpg go h hpp hs ini jade js json less pgp php php4 php5 phps pl py rb scss sh vert vp xml lua log markdown md nfo tex text txt yaml yml)
+set EXT_LIST=(asc bib c cfg coffee conf cpp css ejs frag fp glsl gpg go h hpp hs ini jade js json less org pgp php php4 php5 phps pl py rb scss sh vert vp xml lua log markdown md nfo tex text txt yaml yml)
 ftype txtfile="C:\Program Files (x86)\Vim\vim73\gvim.exe" --remote-silent "%%1"
 
 for %%e in %EXT_LIST% do ( 
